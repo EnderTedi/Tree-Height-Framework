b3f05aee9f1a97c3e39adf47dd4f72cc0e0d5a5d
EnderTedi <endertedi2@gmail.com>
2024-08-20 13:47:39 +0200

    Texture Fixes
    - Fixed crashes relating to being unable to load textures correctly
    - Fixed checks for textures existing

--- ModEntry.cs
+++ ModEntry.cs
@@ -40,19 +40,30 @@ namespace BiggerTrees
             }
         }
 
-        public static Texture2D? TreeTexture(CWildTreeData data)
+        public static Texture2D? TreeTexture(CWildTreeData? wildTreeData = null, CFruitTreeData? fruitTreeData = null)
         {
-            if (data.Textures is null)
+            if (wildTreeData != null)
             {
-                return null;
-            }
-            foreach (CWildTreeData.TextureData data1 in data.Textures)
-            {
-                if (data1.Texture is null) { continue; }
-                if ((data1.Condition == null || GameStateQuery.CheckConditions(data1.Condition)) && (Game1.GetSeasonKeyForLocation(Game1.player.currentLocation).Equals(data1.Season, StringComparison.OrdinalIgnoreCase) || data1.Season == null))
+                if (wildTreeData.Textures is null)
                 {
-                    return Game1.content.Load<Texture2D>(data1.Texture);
+                    return null;
                 }
+                foreach (CWildTreeData.TextureData data1 in wildTreeData.Textures)
+                {
+                    if (data1.Texture is null) { continue; }
+                    if ((data1.Condition == null || GameStateQuery.CheckConditions(data1.Condition)) && (Game1.GetSeasonKeyForLocation(Game1.player.currentLocation).Equals(data1.Season, StringComparison.OrdinalIgnoreCase) || data1.Season == null))
+                    {
+                        if (Game1.content.DoesAssetExist<Texture2D>(data1.Texture))
+                            return Game1.content.Load<Texture2D>(data1.Texture);
+                        else continue;
+                    }
+                }
+            }
+            else if (fruitTreeData != null)
+            {
+                if (fruitTreeData.Texture is null) return null;
+                else if (Game1.content.DoesAssetExist<Texture2D>(fruitTreeData.Texture))
+                    return Game1.content.Load<Texture2D>(fruitTreeData.Texture);
             }
             return null;
         }
@@ -70,15 +81,10 @@ namespace BiggerTrees
                 return true;
             }
             var treeData = tData[__instance.treeType.Value];
-            Texture2D? texture = ModEntry.TreeTexture(treeData);
+            Texture2D? texture = ModEntry.TreeTexture(wildTreeData: treeData);
             Vector2 tileLocation = __instance.Tile;
             float baseSortPosition = __instance.getBoundingBox().Bottom;
-            if (treeData.Textures == null || texture == null)
-            {
-                ModEntry.instance.Monitor.LogOnce($"There are issues with BiggerTree Texture for {__instance.treeType.Value}. Skipping", LogLevel.Error);
-                return true;
-            }
-            else if (__instance.isTemporarilyInvisible || __instance.texture.Value == null || !Tree.TryGetData(__instance.treeType.Value, out var data) || __instance.growthStage.Value < 5)
+            if (texture == null || __instance.isTemporarilyInvisible || __instance.texture.Value == null || !Tree.TryGetData(__instance.treeType.Value, out var data) || __instance.growthStage.Value < 5)
             {
                 return true;
             }
@@ -87,46 +93,32 @@ namespace BiggerTrees
                 if (!__instance.stump.Value || __instance.falling.Value)
                 {
                     if (__instance.IsLeafy())
-                    {
                         spriteBatch.Draw(Game1.mouseCursors, Game1.GlobalToLocal(Game1.viewport, new Vector2(tileLocation.X * 64f - 51f, tileLocation.Y * 64f - 16f)), Tree.shadowSourceRect, Color.White * ((float)Math.PI / 2f - Math.Abs(__instance.shakeRotation)), 0f, Vector2.Zero, 4f, __instance.flipped.Value ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 1E-06f);
-                    }
                     else
-                    {
                         spriteBatch.Draw(Game1.mouseCursors_1_6, Game1.GlobalToLocal(Game1.viewport, new Vector2(tileLocation.X * 64f - 51f, tileLocation.Y * 64f - 16f)), new Rectangle(469, 298, 42, 31), Color.White * ((float)Math.PI / 2f - Math.Abs(__instance.shakeRotation)), 0f, Vector2.Zero, 4f, __instance.flipped.Value ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 1E-06f);
-                    }
+                    
                     Rectangle source_rect = new(0, 0, 48, treeData.TreeHeight * 16);
                     if ((data.UseAlternateSpriteWhenSeedReady && __instance.hasSeed.Value) || (data.UseAlternateSpriteWhenNotShaken && !__instance.wasShakenToday.Value))
-                    {
                         source_rect.X = 48;
-                    }
                     else
-                    {
                         source_rect.X = 0;
-                    }
                     if (__instance.hasMoss.Value)
-                    {
                         source_rect.X = 96;
-                    }
+
                     spriteBatch.Draw(texture, Game1.GlobalToLocal(Game1.viewport, new Vector2(tileLocation.X * 64f + 32f, tileLocation.Y * 64f + 64f - ((treeData.TreeHeight - 6) * 64f))), source_rect, Color.White * __instance.alpha, __instance.shakeRotation, new Vector2(24f, 96f), 4f, __instance.flipped.Value ? SpriteEffects.FlipHorizontally : SpriteEffects.None, (baseSortPosition + 2f) / 10000f - tileLocation.X / 1000000f);
                 }
                 Rectangle stumpSource = Tree.stumpSourceRect;
                 if (__instance.hasMoss.Value)
-                {
                     stumpSource.X += 96;
-                }
                 if (__instance.health.Value >= 1f || (!__instance.falling.Value && __instance.health.Value > -99f))
-                {
                     spriteBatch.Draw(__instance.texture.Value, Game1.GlobalToLocal(Game1.viewport, new Vector2(tileLocation.X * 64f + ((__instance.shakeTimer > 0f) ? ((float)Math.Sin(Math.PI * 2.0 / (double)__instance.shakeTimer) * 3f) : 0f), tileLocation.Y * 64f - 64f)), stumpSource, Color.White * __instance.alpha, 0f, Vector2.Zero, 4f, __instance.flipped.Value ? SpriteEffects.FlipHorizontally : SpriteEffects.None, baseSortPosition / 10000f);
-                }
                 if (__instance.stump.Value && __instance.health.Value < 4f && __instance.health.Value > -99f)
-                {
                     spriteBatch.Draw(__instance.texture.Value, Game1.GlobalToLocal(Game1.viewport, new Vector2(tileLocation.X * 64f + ((__instance.shakeTimer > 0f) ? ((float)Math.Sin(Math.PI * 2.0 / (double)__instance.shakeTimer) * 3f) : 0f), tileLocation.Y * 64f)), new Rectangle(Math.Min(2, (int)(3f - __instance.health.Value)) * 16, 144, 16, 16), Color.White * __instance.alpha, 0f, Vector2.Zero, 4f, __instance.flipped.Value ? SpriteEffects.FlipHorizontally : SpriteEffects.None, (baseSortPosition + 1f) / 10000f);
-                }
             }
+
             foreach (Leaf i in ___leaves)
-            {
                 spriteBatch.Draw(__instance.texture.Value, Game1.GlobalToLocal(Game1.viewport, i.position), new Rectangle(16 + i.type % 2 * 8, 112 + i.type / 2 * 8, 8, 8), Color.White, i.rotation, Vector2.Zero, 4f, SpriteEffects.None, baseSortPosition / 10000f + 0.01f);
-            }
+
             return false;
         }
     }
@@ -138,24 +130,17 @@ namespace BiggerTrees
         {
             var tData = Game1.content.Load<Dictionary<string, CFruitTreeData>>($"{ModEntry.instance.ModManifest.UniqueID}/FruitTreeData");
             if (!tData.ContainsKey(__instance.treeId.Value))
-            {
                 return true;
-            }
+
             var treeData = tData[__instance.treeId.Value];
-            Texture2D customTexture = Game1.content.Load<Texture2D>(treeData.Texture);
+            Texture2D? customTexture = ModEntry.TreeTexture(fruitTreeData: treeData);
+            if (customTexture == null || __instance.isTemporarilyInvisible || __instance.texture == null || __instance.growthStage.Value < 4)
+                return true;
+
             int seasonIndex = Game1.GetSeasonIndexForLocation(__instance.Location);
             int spriteRow = __instance.GetSpriteRowNumber();
             Vector2 tileLocation = __instance.Tile;
             Rectangle boundingBox = __instance.getBoundingBox();
-            if (treeData.Texture == null || customTexture == null)
-            {
-                ModEntry.instance.Monitor.LogOnce($"There are issues with BiggerTree Texture for {__instance.treeId.Value}. Skipping", LogLevel.Error);
-                return true;
-            }
-            else if (__instance.isTemporarilyInvisible || __instance.texture == null || __instance.growthStage.Value < 4)
-            {
-                return true;
-            }
 
             if (__instance.GreenHouseTileTree)
             {
@@ -166,16 +151,12 @@ namespace BiggerTrees
             {
                 bool ignoreSeason = __instance.IgnoresSeasonsHere();
                 if (!__instance.falling.Value)
-                {
                     spriteBatch.Draw(customTexture, Game1.GlobalToLocal(Game1.viewport, new Vector2(tileLocation.X * 64f + 32f, tileLocation.Y * 64f + 64f)), new Rectangle(ignoreSeason ? 1 : seasonIndex * 3 * 16, 0 + (treeData.TreeHeight - 1) * 16, 48, 16), (__instance.struckByLightningCountdown.Value > 0) ? (Color.Gray * __instance.alpha) : (Color.White * __instance.alpha), 0f, new Vector2(24f, 16f), 4f, __instance.flipped.Value ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 1E-07f);
-                }
                 spriteBatch.Draw(customTexture, Game1.GlobalToLocal(Game1.viewport, new Vector2(tileLocation.X * 64f + 32f, tileLocation.Y * 64f + 64f - ((treeData.TreeHeight - 5) * 64f))), new Rectangle(ignoreSeason ? 1 : seasonIndex * 3 * 16, 0, 48, (treeData.TreeHeight - 1) * 16), (__instance.struckByLightningCountdown.Value > 0) ? (Color.Gray * __instance.alpha) : (Color.White * __instance.alpha), __instance.shakeRotation, new Vector2(24f, 80f), 4f, __instance.flipped.Value ? SpriteEffects.FlipHorizontally : SpriteEffects.None, (float)boundingBox.Bottom / 10000f + 0.001f - tileLocation.X / 1000000f);
             }
 
             if (__instance.health.Value >= 1f || (!__instance.falling.Value && __instance.health.Value > -99f))
-            {
                 spriteBatch.Draw(__instance.texture, Game1.GlobalToLocal(Game1.viewport, new Vector2(tileLocation.X * 64f + 32f + ((__instance.shakeTimer > 0f) ? ((float)Math.Sin(Math.PI * 2.0 / __instance.shakeTimer) * 2f) : 0f), tileLocation.Y * 64f + 64f)), new Rectangle(384, spriteRow * 5 * 16 + 48, 48, 32), (__instance.struckByLightningCountdown.Value > 0) ? (Color.Gray * __instance.alpha) : (Color.White * __instance.alpha), 0f, new Vector2(24f, 32f), 4f, __instance.flipped.Value ? SpriteEffects.FlipHorizontally : SpriteEffects.None, (__instance.stump.Value && !__instance.falling.Value) ? (boundingBox.Bottom / 10000f) : (boundingBox.Bottom / 10000f - 0.001f - tileLocation.X / 1000000f));
-            }
 
             for (int i = 0; i < __instance.fruit.Count; i++)
             {
@@ -196,9 +177,8 @@ namespace BiggerTrees
                 }
             }
             foreach (Leaf j in ___leaves)
-            {
                 spriteBatch.Draw(__instance.texture, Game1.GlobalToLocal(Game1.viewport, j.position), new Rectangle((24 + seasonIndex) * 16, spriteRow * 5 * 16, 8, 8), Color.White, j.rotation, Vector2.Zero, 4f, SpriteEffects.None, (float)boundingBox.Bottom / 10000f + 0.01f);
-            }
+
             return false;
         }
     }
